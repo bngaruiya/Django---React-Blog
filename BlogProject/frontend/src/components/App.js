f9196caf6b95dcb9f4340d8d62363d3cfc379e19
Bernard Ngaruiya <bngaruiya@gmail.com>
2020-02-03 22:50:03 +0300

    Feat: Added React Authentication and PrivateRoutes

--- BlogProject/frontend/src/components/App.js
+++ BlogProject/frontend/src/components/App.js
@@ -3,11 +3,21 @@ import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 
 import Header from '../layout/Header';
 import Dashboard from './posts/Dashboard';
 import store from '../store';
 import Alerts from '../layout/Alerts';
+import Login from './accounts/Login';
+import Register from './accounts/Register';
+import PrivateRoute from './common/PrivateRoute';
+import { loaduser } from '../actions/auth';
 
 // Alert Options
 const alertOptions = {
@@ -16,17 +26,26 @@ const alertOptions = {
 };
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loaduser());
+  }
   render() {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Fragment>
-            <Header />
-            <Alerts />
-            <div className='container'>
-              <Dashboard />
-            </div>
-          </Fragment>
+          <Router>
+            <Fragment>
+              <Header />
+              <Alerts />
+              <div className='container'>
+                <Switch>
+                  <PrivateRoute exact path='/' component={Dashboard} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
         </AlertProvider>
       </Provider>
     );
--- BlogProject/frontend/src/layout/Toolbar/Toolbar.js
+++ BlogProject/frontend/src/layout/Toolbar/Toolbar.js
@@ -1,33 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 import './Toolbar.css';
 
-const toolbar = props => (
-  <header className="toolbar">
-    <nav className="toolbar_navigation">
-      <div className="toolbar_toggle_button">
-        <DrawerToggleButton click={props.drawerClickHandler} />
-      </div>
-      <div className="toolbar_logo">
-        <a href="/">THE LOGO</a>
-      </div>
-      <div className="logo_navitems_spacer" />
-      <div className="toolbar_navigation_items">
-        <ul>
-          <li>
-            <a href="/">Articles</a>
-          </li>
-          <li>
-            <a href="/">Notebooks</a>
-          </li>
-          <li>
-            <a href="/">About</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-);
+class Toolbar extends Component {
+  render() {
+    return (
+      <header className='toolbar'>
+        <nav className='toolbar_navigation'>
+          <div className='toolbar_toggle_button'>
+            <DrawerToggleButton click={this.props.drawerClickHandler} />
+          </div>
+          <div className='toolbar_logo'>
+            <a href='/'>THE LOGO</a>
+          </div>
+          <div className='logo_navitems_spacer' />
+          <div className='toolbar_navigation_items'>
+            <ul>
+              <li>
+                <a href='/'>Articles</a>
+              </li>
+              <li>
+                <a href='/'>About</a>
+              </li>
+              <li>
+                <Link to='/register'>Register</Link>
+              </li>
+              <li>
+                <Link to='/login'>Login</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
 
-export default toolbar;
+export default Toolbar;
